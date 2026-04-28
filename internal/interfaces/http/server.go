012783db3f092351ec867f94118eeb91d779407d
pedramcode <dev.dehghanpour@gmail.com>
2026-04-28 08:17:58 +0330

    graceful shutdown

--- internal/interfaces/http/server.go
+++ internal/interfaces/http/server.go
@@ -1,23 +1,39 @@
 package http
 
 import (
+	"context"
 	"dovenet/user-service/internal/application"
 	"dovenet/user-service/internal/interfaces/http/router"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
 type HttpServer struct {
+	server *http.Server
 	engine *gin.Engine
 	logger *zap.Logger
 }
 
 func NewHttpServer(logger *zap.Logger, userService *application.UserService) *HttpServer {
+	engine := gin.Default()
+	addr := fmt.Sprintf("%s:%s", os.Getenv("HTTP_HOST"), os.Getenv("HTTP_PORT"))
+
 	server := &HttpServer{
-		engine: gin.Default(),
+		engine: engine,
+		server: &http.Server{
+			Addr:         addr,
+			Handler:      engine,
+			ReadTimeout:  15 * time.Second,
+			WriteTimeout: 15 * time.Second,
+			IdleTimeout:  60 * time.Second,
+		},
 		logger: logger,
 	}
 
@@ -26,6 +42,29 @@ func NewHttpServer(logger *zap.Logger, userService *application.UserService) *Ht
 	return server
 }
 
-func (s *HttpServer) Run() {
-	s.engine.Run(fmt.Sprintf("%s:%s", os.Getenv("HTTP_HOST"), os.Getenv("HTTP_PORT")))
+func (s *HttpServer) Run() error {
+	go func() {
+		s.logger.Info("Starting HTTP server: ", zap.String("addr", s.server.Addr))
+		if err := s.server.ListenAndServe(); err != nil {
+			s.logger.Fatal("Failed to start server", zap.Error(err))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	s.logger.Info("Shutting down server...")
+
+	// Graceful shutdown with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.server.Shutdown(ctx); err != nil {
+		s.logger.Error("Server forced to shutdown", zap.Error(err))
+		return err
+	}
+
+	s.logger.Info("Server exited gracefully")
+	return nil
 }
